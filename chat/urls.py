a27ed40e20b5db3653272256e600eb277da25e2c
alaaabdelkareem <alaa.abdelkareem94@gmail.com>
2018-05-24 17:23:28 +0200

    bug fixed

--- chat/urls.py
+++ chat/urls.py
@@ -1,17 +1,16 @@
 from django.contrib.auth.views import logout
+from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.forms import UserCreationForm
-from django.conf.urls import url , include
+
 
 urlpatterns = [
-    url(r'', views.index, name='index'),
-    url(r'^chat', views.chat_view, name='chats'),
-    url(r'^chat/<int:sender>/<int:receiver>', views.message_view, name='chat'),
-    url(r'^api/messages/<int:sender>/<int:receiver>', views.message_list, name='message-detail'),
-    url(r'^api/messages', views.message_list, name='message-list'),
-    url(r'^api/users/<int:pk>', views.user_list, name='user-detail'),
-    url(r'^api/users', views.user_list, name='user-list'),
-    url(r'^logout', logout, {'next_page': 'index'}, name='logout'),
-    url(r'^register', views.register_view, name='register'),
+    path('', views.index, name='index'),
+    path('chat', views.chat_view, name='chats'),
+    path('chat/<int:sender>/<int:receiver>', views.message_view, name='chat'),
+    path('api/messages/<int:sender>/<int:receiver>', views.message_list, name='message-detail'),
+    path('api/messages', views.message_list, name='message-list'),
+    path('api/users/<int:pk>', views.user_list, name='user-detail'),
+    path('api/users', views.user_list, name='user-list'),
+    path('logout', logout, {'next_page': 'index'}, name='logout'),
+    path('register', views.register_view, name='register'),
 ]
